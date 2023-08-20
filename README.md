614b6b96d809727ff625f567588b6c661068a3dd
Rafael Mazzutti <rafaelmazz22@gmail.com>
2023-08-20 13:19:22 -0300

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+nixos config
+[-](https://github.com/Misterio77/nix-starter-configs).
+
+rn only for my personal notebook ·͜·
+I will fix and clean it later
---
+++ home-manager/home.nix
@@ -0,0 +1,100 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+
+{ inputs, lib, config, pkgs, ... }: 
+{
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModule
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  home = {
+    username = "rafameou";
+    homeDirectory = "/home/rafameou";
+  };
+
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  home.packages = with pkgs; [
+    # ---- internet ----
+    vivaldi
+    google-chrome
+    thunderbird
+    tdesktop
+    (discord.override { nss = nss_latest; })
+    #(discord-canary.override { nss = nss_latest; })
+    spotify
+    bitwarden
+    whatsapp-for-linux
+    # ---- edit ----
+    gimp
+    onlyoffice-bin
+    libreoffice-qt
+    hunspell
+    hunspellDicts.pt_BR
+    strawberry
+    mpv
+    #texlive.combined.scheme-small
+    (texlive.combine {inherit (texlive) scheme-small abnt abntex2 collection-langportuguese dvipng;})
+    cherrytree
+    texstudio
+    # ---- utils ----
+    gnome.gnome-software
+    libsForQt5.discover
+    flatpak
+    github-desktop
+    vscode-with-extensions
+    alacritty
+    octaveFull
+    nicotine-plus
+    qbittorrent
+    neovim
+    neovim-qt
+    unar
+    unzip
+    zip
+    xsane
+    htop
+    # ---- games ----
+    easyrpg-player
+    # ---- extra ----
+    nixos-icons
+    mesa-demos
+  ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  #programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+   
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
