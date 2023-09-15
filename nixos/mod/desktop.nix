5764e8d6a4749ca3b45b5ad015d12e6e6e019cb2
Rafael Mazzutti <rafaelmazz22@gmail.com>
2023-09-15 15:18:30 -0300

    :aries:

--- nixos/mod/desktop.nix
+++ nixos/mod/desktop.nix
@@ -6,6 +6,7 @@
   # GNOME
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.flashback.enableMetacity = true;
 
   # Configure keymap in X11
   services.xserver = {
