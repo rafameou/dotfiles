784bca8c7dec98b69ab5297b25296c4c5c943880
Rafael Mazzutti <rafaelmazz22@gmail.com>
2023-12-20 23:55:53 -0300

    Update qutebrowser.nix

--- home-manager/mod/qutebrowser.nix
+++ home-manager/mod/qutebrowser.nix
@@ -2,7 +2,8 @@
 {
   programs.qutebrowser = {
     enable = true;
-    package = (qutebrowser.override { enableWideVine = true; });
+    package = (pkgs.qutebrowser.override { enableWideVine = true; });
+    loadAutoconfig = true;
     searchEngines = {
       DEFAULT = "https://www.google.com/search?hl=en&q={}";
       w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";  
@@ -10,15 +11,11 @@
       nw = "https://nixos.wiki/index.php?search={}";  
       g = "https://www.google.com/search?hl=en&q={}";
     };
-    settings = ''
+    settings = {
+      url.start_pages = "about:blank";
+    };
+    extraConfig = ''
       import catppuccin
-
-      # load your autoconfig, use this, if the rest of your config is empty!
-      config.load_autoconfig()
-
-      # set the flavor you'd like to use
-      # valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
-      # last argument (optional, default is False): enable the plain look for the menu rows
       catppuccin.setup(c, 'frappe', True)
     '';
   };
