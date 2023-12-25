da8e76ca9f02479ab918a054e3d3607f7655a552
Rafael Mazzutti <rafaelmazz22@gmail.com>
2023-12-24 23:13:21 -0300

    Update qutebrowser.nix

--- home-manager/mod/qutebrowser.nix
+++ home-manager/mod/qutebrowser.nix
@@ -1,22 +1,34 @@
 { inputs, lib, config, pkgs, ... }:
-{
-  programs.qutebrowser = {
-    enable = true;
-    package = (pkgs.qutebrowser.override { enableWideVine = true; });
-    loadAutoconfig = true;
-    searchEngines = {
-      DEFAULT = "https://www.google.com/search?hl=en&q={}";
-      w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";  
-      aw = "https://wiki.archlinux.org/?search={}";  
-      nw = "https://nixos.wiki/index.php?search={}";  
-      g = "https://www.google.com/search?hl=en&q={}";
+let catpuccin = pkgs.fetchFromGitHub {
+  owner = "catppuccin";
+  repo = "qutebrowser";
+  rev = "78bb72b4c60b421c8ea64dd7c960add6add92f83";
+  hash = "sha256-lp7HWYuD4aUyX1nRipldEojZVIvQmsxjYATdyHWph0g=";
+};
+in
+  {
+    xdg.configFile."qutebrowser/catppuccin".source = catpuccin;
+    programs.qutebrowser = {
+      enable = true;
+      package = (pkgs.qutebrowser.override { enableWideVine = true; });
+      loadAutoconfig = true;
+      searchEngines = {
+        DEFAULT = "https://www.google.com/search?hl=en&q={}";
+        w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";  
+        aw = "https://wiki.archlinux.org/?search={}";  
+        nw = "https://nixos.wiki/index.php?search={}";  
+        g = "https://www.google.com/search?hl=en&q={}";
+      };
+      settings = {
+        url.start_pages = "about:blank";
+        tabs.show = "multiple";
+        /* tabs.tabs_are_windows = true;*/
+        statusbar.show = "in-mode";
+        fonts.default_family = "Inter";
+      };
+      extraConfig = ''
+        import catppuccin
+        catppuccin.setup(c, 'frappe', True)
+      '';
     };
-    settings = {
-      url.start_pages = "about:blank";
-    };
-    extraConfig = ''
-      import catppuccin
-      catppuccin.setup(c, 'frappe', True)
-    '';
-  };
-}
+  }
