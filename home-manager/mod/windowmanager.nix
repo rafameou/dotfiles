168c5195010b6c13ef4bef21c8bd2567056437f9
Rafael Mazzutti <rafaelmazz22@gmail.com>
2024-01-03 11:02:00 -0300

    Startx and Starti3

--- home-manager/mod/windowmanager.nix
+++ home-manager/mod/windowmanager.nix
@@ -29,7 +29,23 @@
   programs.zsh = {
     loginExtra = ''
       export PATH="$HOME/.local/bin:$PATH" ;
-      if [ "$(tty)" = "/dev/tty1" ]; then
+    '';
+  };
+
+  home.file = {
+    ".local/bin/prime-run" = {
+      text = ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec "$@"
+      '';
+      executable = true;
+    };
+    ".local/bin/startsway" = {
+      text = ''
+        #!/bin/sh
         # https://www.reddit.com/r/swaywm/comments/11d89w2/some_workarounds_to_use_sway_with_nvidia/
         # export WLR_RENDERER=vulkan ;
         # export WLR_NO_HARDWARE_CURSORS=1 ; # breaks steam
@@ -43,11 +59,33 @@
         export CLUTTER_BACKEND="wayland" ; 
         export XDG_SESSION_TYPE="wayland" ;
         exec sway --unsupported-gpu ;
-      fi
-      if [ "$(tty)" = "/dev/tty2" ]; then
-        startx ;
-      fi
-    '';
+      '';
+      executable = true;
+    };
+    ".xinitrc" = {
+      text = ''
+        exec i3
+      '';
+    };
+    ".local/bin/starti3" = {
+      text = ''
+        #!/bin/sh
+        # https://www.reddit.com/r/swaywm/comments/11d89w2/some_workarounds_to_use_sway_with_nvidia/
+        # unset WLR_RENDERER ;
+        # unset WLR_NO_HARDWARE_CURSORS ; # breaks steam
+        # unset XWAYLAND_NO_GLAMOR ;
+        unset SDL_VIDEODRIVER ;
+        unset QT_QPA_PLATFORM ;
+        export QT_QPA_PLATFORMTHEME=qt5ct ; 
+        unset QT_WAYLAND_DISABLE_WINDOWDECORATION ;
+        unset _JAVA_AWT_WM_NONREPARENTING ;
+        unset MOZ_ENABLE_WAYLAND ;
+        unset CLUTTER_BACKEND ; 
+        unset XDG_SESSION_TYPE ;
+        exec startx ;
+      '';
+      executable = true;
+    };
   };
 
   services.dunst = {
@@ -61,4 +99,4 @@
   };
 
   /*services.udisks2.enable = true; services.playerctld.enable = true;*/
- }
+}
--- nixos/mod/env.nix
+++ nixos/mod/env.nix
@@ -2,12 +2,11 @@
 {
   #esc + v + esc ou ctrl + v + esc
   environment.etc."issue".text = ''
-    [1;32m<<< Bem-Vindo a \n! >>[0m
-    NixOS com GNU/Linux \r (\m)
+[1;32m<<< Bem-Vindo a \n! >>[0m
+NixOS com GNU/Linux \r (\m)
 
-    [1;36mHoje é \d, agora sao \t e voce esta na \l[0m
-    Caso voce tenha encontrado esse computador sozinho aleatoriamente por ai, contate: 
-    +55 (42) 9 8402-6139 (telefone, whatsapp ou telegram) ou rafaelmazz22@gmail.com
-
+[1;36mHoje é \d, agora sao \t e voce esta na \l[0m
+Caso voce tenha encontrado esse computador sozinho aleatoriamente por ai, contate: 
++55 (42) 9 8402-6139 (telefone, whatsapp ou telegram) ou rafaelmazz22@gmail.com
   '';
 }
