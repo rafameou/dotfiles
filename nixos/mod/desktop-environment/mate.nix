c40472a08a5995d67d389f839868164e313f32fd
Rafael Mazzutti <rafaelmazz22@gmail.com>
2024-03-06 12:02:01 -0300

    forgot to git add whoops

---
+++ nixos/mod/desktop-environment/mate.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  servoces.xserver = true;
+  #services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.mate.enable = true;
+
+  services.gnome.gnome-keyring.enable = true;
+
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "mate-session";
+  #services.xrdp.openFirewall = true;
+
+  # flatpak
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "*";
+  };
+}
