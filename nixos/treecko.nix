ac45a4bc803d34e5f45e994dac5986346efe9128
Rafael Mazzutti <rafaelmazz22@gmail.com>
2024-03-20 22:24:03 -0300

    changes to treecko

--- nixos/treecko.nix
+++ nixos/treecko.nix
@@ -10,7 +10,8 @@
     ./mod/zsh.nix
     ./mod/desktop.nix
     ./mod/qemu.nix
-    ./mod/desktop-environment/kde.nix 
+    ./mod/sway.nix
+    ./mod/desktop-environment/mate.nix 
     ./mod/pipewire.nix
     ./mod/ssd.nix
     ./mod/time.nix
