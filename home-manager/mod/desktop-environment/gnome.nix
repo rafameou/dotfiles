2cae5def2481239cac78c2b9e9424ab35b1264c8
Rafael Mazzutti <rafaelmazz22@gmail.com>
2024-04-16 23:57:15 -0300

    gnome keybinds

--- home-manager/mod/desktop-environment/gnome.nix
+++ home-manager/mod/desktop-environment/gnome.nix
@@ -4,14 +4,13 @@
     gnome.gnome-software
     gnome.gnome-tweaks
     gnome.dconf-editor
-    gnome-builder
+    #gnome-builder
     # ---- gnome extensions ----
     gnomeExtensions.runcat
     gnomeExtensions.appindicator
     gnomeExtensions.openweather
     gnomeExtensions.alphabetical-app-grid
     gnomeExtensions.favourites-in-appgrid
-    gnomeExtensions.todotxt
     gnomeExtensions.hot-edge
     gnomeExtensions.caffeine
     gnomeExtensions.activate_gnome
@@ -20,6 +19,9 @@
     adwaita-qt6
     libsForQt5.qt5.qtwayland
     kdePackages.qtwayland
+
+    wl-clipboard 
+    wl-clipboard-x11
   ];
 
   qt = {
@@ -29,24 +31,63 @@
   };
 
   dconf.settings = {
+    "org/gnome/mutter" = {
+      dynamic-workspaces = true;
+    };
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = ["<Super><Shift>1"];
+      switch-to-application-2 = ["<Super><Shift>2"];
+      switch-to-application-3 = ["<Super><Shift>3"];
+      switch-to-application-4 = ["<Super><Shift>4"];
+      switch-to-application-5 = ["<Super><Shift>5"];
+      switch-to-application-6 = ["<Super><Shift>6"];
+      switch-to-application-7 = ["<Super><Shift>7"];
+      switch-to-application-8 = ["<Super><Shift>8"];
+      switch-to-application-9 = ["<Super><Shift>9"];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      close = ["<Alt>F4" "<Shift><Super>q"];
+      switch-to-workspace-1 = ["<Super>Home" "<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+      switch-to-workspace-5 = ["<Super>5"];
+      switch-to-workspace-6 = ["<Super>6"];
+      switch-to-workspace-7 = ["<Super>7"];
+      switch-to-workspace-8 = ["<Super>8"];
+      switch-to-workspace-9 = ["<Super>9"];
+    };
+    "org/gnome/shell/window-switcher" = {
+      current-workspace-only = true;
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+      titlebar-font = "Fira Sans 11";
+    };
+    "org/gnome/desktop/interface" = {
+      clock-show-seconds = true;
+      show-battery-percentage = true;
+      font-name = "Fira Sans 11";
+      document-font-name = "Fira Sans 11";
+    };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+    };
+    "org/gtk/settings/file-chooser" = {
+      sort-directories-first = true;
+    };
+    "org/gtk/gtk4/settings/file-chooser" = {
+      sort-directories-first = true;
+    };
     "org/gnome/shell" = {
-      /*favorite-apps = [
-      "vivaldi-stable.desktop"
-      "google-chrome.desktop"
-      "thunderbird.desktop"
-      "org.gnome.Nautilus.desktop"
-      "cherrytree.desktop"
-      "org.telegram.desktop.desktop"
-      "discord.desktop"
-      "vivaldi-hnpfjngllnobngcgfapefoaidbinmjnm-Default.desktop" #whatsapp
-      "vivaldi-akpamiohjfcnimfljfndmaldlcfphjmp-Default.desktop" #instagram
-      "vivaldi-jneocipojkkahfcibhjaiilegofacenn-Default.desktop" #duolingo
-      "spotify.desktop"
-      "bitwarden.desktop"
-      "org.gnome.Console.desktop"
-      "gnome-system-monitor.desktop"
-      "github-desktop.desktop"
-      ];*/
+      favorite-apps = [
+        "org.qutebrowser.qutebrowser.desktop"
+        "google-chrome.desktop"
+        "org.codeberg.dnkl.foot.desktop"
+        "org.gnome.Nautilus.desktop"
+        "org.telegram.desktop.desktop"
+        "vesktop.desktop"
+      ];
       disable-user-extensions = false;
       enabled-extensions = [
         "runcat@kolesnikov.se"
@@ -54,7 +95,6 @@
         "openweather-extension@jenslody.de"
         "AlphabeticalAppGrid@stuarthayhurst"
         "favourites-in-appgrid@harshadgavali.gitlab.org"
-        "todo.txt@bart.libert.gmail.com"
         "hotedge@jonathan.jdoda.ca"
         "caffeine@patapon.info"
       ];
@@ -110,25 +150,6 @@
     "org/gnome/desktop/app-folders/folders/wine" = {
       name = "Wine";
       categories = ["Wine" "X-Wine" "Wine-Programs-Accessories"];
-    };
-    "org/gnome/desktop/wm/preferences" = {
-      button-layout = "appmenu:minimize,maximize,close";
-      titlebar-font = "Fira Sans 11";
-    };
-    "org/gnome/desktop/interface" = {
-      clock-show-seconds = true;
-      show-battery-percentage = true;
-      font-name = "Fira Sans 11";
-      document-font-name = "Fira Sans 11";
-    };
-    "org/gnome/desktop/peripherals/touchpad" = {
-      tap-to-click = true;
-    };
-    "org/gtk/settings/file-chooser" = {
-      sort-directories-first = true;
-    };
-    "org/gtk/gtk4/settings/file-chooser" = {
-      sort-directories-first = true;
     };
   };
 }
