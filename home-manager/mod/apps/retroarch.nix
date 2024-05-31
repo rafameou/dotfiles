bb7661e94e38d86633df5c714fe7d68964463a78
Rafael Mazzutti <rafaelmazz22@gmail.com>
2024-05-30 22:28:44 -0300

    nvidia (x11 only), retroarch cores

--- home-manager/mod/apps/retroarch.nix
+++ home-manager/mod/apps/retroarch.nix
@@ -3,8 +3,10 @@
   home.packages = with pkgs; [
     (retroarch.override {
       cores = with libretro; [
+        blastem
         beetle-psx-hw
         dolphin
+        fceumm
         parallel-n64
         ppsspp
         snes9x
--- nixos/mod/nvidia-hell.nix
+++ nixos/mod/nvidia-hell.nix
@@ -1,23 +1,17 @@
 { pkgs, ... }:
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in
 {
   #"open with dedicated graphics card" on gnome
   services.switcherooControl.enable = true;
 
-  
   # nvidia prime
   services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.prime = {
-    offload.enable = true;
-    #sync.enable = true;
+    #offload.enable = true;
+    sync.enable = true;
+    #reverseSync.enable = true;
+
+    #offload.enableOffloadCmd = true;
 
     # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
     intelBusId = "PCI:0:2:0";
