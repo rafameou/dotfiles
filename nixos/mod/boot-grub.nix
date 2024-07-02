ecf1f16518916ce20d61d44c59d17424f71c3376
Rafael Mazzutti <rafaelmazz22@gmail.com>
2024-07-01 21:55:21 -0300

    dualboot on bulbasaur

--- nixos/mod/boot-grub.nix
+++ nixos/mod/boot-grub.nix
@@ -3,6 +3,27 @@
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.useOSProber = false;
   boot.kernelPackages = pkgs.linuxPackages_cachyos;#_latest;
+
+  boot.loader.grub.extraEntries = ''
+menuentry 'Windows 10' --class windows {
+	insmod part_msdos
+	insmod ntfs
+	set root='hd1,msdos1'
+	parttool ${"$"}{root} hidden-
+	drivemap -s (hd0) ${"$"}{root}
+	chainloader +1
+}
+
+menuentry 'Windows XP' --class windows {
+	insmod part_msdos
+	insmod ntfs
+        insmod ntldr
+        set root='hd1,msdos1'
+	parttool ${"$"}{root} hidden-
+	drivemap -s (hd0) ${"$"}{root}
+	ntldr /ntldr
+}
+  '';
 }
