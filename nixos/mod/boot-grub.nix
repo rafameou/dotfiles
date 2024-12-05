6695b6c533fdc9237379de6975f1e52067818894
Rafael Mazzutti <rafaelmazz22@gmail.com>
2024-12-05 10:22:37 -0300

    enable osprober

--- nixos/mod/boot-grub.nix
+++ nixos/mod/boot-grub.nix
@@ -3,7 +3,7 @@
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = false;
+  boot.loader.grub.useOSProber = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   /*boot.loader.grub.extraEntries = ''
