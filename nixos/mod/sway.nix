ce6cfc6cbb79a63554e8a1b426ef864b503f743e
Rafael Mazzutti <rafaelmazz22@gmail.com>
2024-12-14 00:33:47 -0300

    some changes on ly

--- nixos/mod/sway.nix
+++ nixos/mod/sway.nix
@@ -2,7 +2,16 @@
 {
   services.xserver.displayManager.startx.enable = true;
   services.xserver.displayManager.lightdm.enable = false;
-  services.displayManager.ly.enable = true;
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "doom";
+      clock = "%c";
+      initial_info_text = "(42) 98402-6139 rafameow@proton.me";
+      vi_mode = "true";
+      min_refresh_delta = "500";
+    };
+  };
   #enable sway
   programs.sway = {
     enable = true;
