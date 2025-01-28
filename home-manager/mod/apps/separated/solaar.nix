91283586d0e785479fc54225db66fc59a0ad0495
Rafael Mazzutti <rafaelmazz22@gmail.com>
2025-01-27 23:06:18 -0300

    fix solaar

--- home-manager/mod/apps/separated/solaar.nix
+++ home-manager/mod/apps/separated/solaar.nix
@@ -6,38 +6,44 @@
       ---
       - Key: [Emoji Smiley Heart Eyes, pressed]
       - KeyPress:
-      - KP_Home
-      - click
+        - KP_Home
+        - click
       ...
       ---
       - Key: [Emoji Crying Face, pressed]
       - KeyPress:
-      - KP_Page_Up
-      - click
+        - KP_Page_Up
+        - click
       ...
       ---
       - Key: [Emoji Smiley, pressed]
       - KeyPress:
-      - KP_Page_Down
-      - click
+        - KP_Page_Down
+        - click
       ...
       ---
       - Key: [Emoji Smilie With Tears, pressed]
       - KeyPress:
-      - KP_End
-      - click
+        - KP_End
+        - click
       ...
       ---
       - Key: [Open Emoji Panel, pressed]
       - KeyPress:
-      - backslash
-      - click
+        - backslash
+        - click
       ...
       ---
       - Key: [Voice Dictation, pressed]
       - KeyPress:
-      - backslash
-      - click
+        - backslash
+        - click
+      ...
+      ---
+      - Key: [Snipping Tool, pressed]
+      - KeyPress:
+        - Print
+        - click
       ...
     '';
   };
