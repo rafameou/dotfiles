1ae900cbe9c71b48f55e552271d4582a31ffd6ec
Rafael Mazzutti <rafaelmazz22@gmail.com>
2025-06-26 12:29:43 -0300

    treecko

--- nixos/home-manager/treecko.nix
+++ nixos/home-manager/treecko.nix
@@ -53,4 +53,9 @@
     "eDP-1, preferred, auto, 1.5"
     ", preferred, auto, 1"
   ];
+
+  programs.waybar.settings.bottomBar = {
+    cpu.format = lib.mkForce "{icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7}{usage}%";
+    temperature.hmon-path = lib.mkForce "/sys/devices/platform/thinkpad_hwmon/hwmon/hwmon6/temp1_input";
+  };
 }
