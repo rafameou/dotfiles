9b2569a0aa46c9ad88cbaa3ce099af34245ed840
Rafael Mazzutti <rafaelmazz22@gmail.com>
2025-06-29 22:05:11 -0300

    removing xserver from gnome service name
    evaluation warning: The option `services.xserver.desktopManager.gnome.enable' defined in `/nix/store/z7bbgiw04qhrli8zrhyrp5lrdxna2600-source/nixos/system/mod/desktop-environment/gnome.nix' has been renamed to `services.desktopManager.gnome.enable'.
    evaluation warning: The option `services.xserver.displayManager.gdm.enable' defined in `/nix/store/z7bbgiw04qhrli8zrhyrp5lrdxna2600-source/nixos/system/mod/desktop-environment/gnome.nix' has been renamed to `services.displayManager.gdm.enable'.

--- nixos/system/mod/desktop-environment/gnome.nix
+++ nixos/system/mod/desktop-environment/gnome.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   #services.xserver.desktopManager.gnome.flashback.enableMetacity = true;
   services.gnome.games.enable = true;
 
