457ad39c13dddb125f0654704f3f1a83ebf96b69
Rafael Mazzutti <rafaelmazz22@gmail.com>
2025-08-19 10:56:24 -0300

    readded darktable, added insecure package for c...
    isco packet tracer

--- nixos/home-manager/mod/apps/edit.nix
+++ nixos/home-manager/mod/apps/edit.nix
@@ -14,10 +14,14 @@
   #   package = pkgs.zed-editor-fhs;
   # };
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "libxml2-2.13.8" # packettracer
+  ];
+
   home.packages = with pkgs; [
     nixfmt-rfc-style
     gimp3
-    #darktable #broken 29-07-2025, libsoup
+    darktable
     onlyoffice-bin
     libreoffice-qt-fresh
     hunspell
@@ -25,7 +29,7 @@
     corefonts
     vistafonts
     #feishin #broken 22-01-2025, electron, not needed
-    strawberry #broken 18-07-2025
+    strawberry # broken 18-07-2025
     #texlive.combined.scheme-full
     cherrytree
     imagemagick
