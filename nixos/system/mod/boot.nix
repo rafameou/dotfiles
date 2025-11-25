e7cc54417431633dfe598f643f759521754eba02
Rafael Mazzutti <rafaelmazz22@gmail.com>
2025-11-25 20:14:50 -0300

    bootloader

--- nixos/system/mod/boot.nix
+++ nixos/system/mod/boot.nix
@@ -1,13 +1,18 @@
 { pkgs, ... }:
 {
   # Bootloader.
-  boot.loader.systemd-boot.enable = true; # lib.mkForce false; #true;
-  #boot.lanzaboote = {
-  #  enable = true;
-  #  pkiBundle = "/etc/secureboot";
-  #};
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.bootspec.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.plymouth.enable = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true; # lib.mkForce false; #true;
+      efi.canTouchEfiVariables = true;
+      timeout = 0;
+    };
+    #lanzaboote = {
+    #  enable = true;
+    #  pkiBundle = "/etc/secureboot";
+    #};
+    bootspec.enable = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+    plymouth.enable = true;
+  };
 }
